ff2a195099ebc87152423ad2f2b30ed014c18ad1
takjehyeong <164008835+takjehyeong@users.noreply.github.com>
2024-04-17 18:09:22 +0900

    Add files via upload

---
+++ 0402/main.c
@@ -0,0 +1,11 @@
+#include"hello.h"
+#include"world.h"
+
+int main(){
+	
+	say_hello();
+	say_world();
+
+	return 0;
+
+}
---
+++ 0402/world.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"world.h"
+
+void say_world(){
+
+	printf("World!!\n");
+
+}
