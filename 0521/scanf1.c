9efeaab647f4ea1e618ba489ce1b39d5329b9dd7
takjehyeong <164008835+takjehyeong@users.noreply.github.com>
2024-05-28 17:06:26 +0900

    Add files via upload

---
+++ 0521/scanf1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int numinput;
+	char charinput;
+	scanf("%d", &numinput);
+	printf("%d\n", numinput);
+	
+	scanf(" %c" ,&charinput);
+	printf("%c", charinput);
+	//예상 결과는 ??
+	return 0;
+}
